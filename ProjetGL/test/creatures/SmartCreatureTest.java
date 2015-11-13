d6e503046c4c3e10799cd5ba25dca14b92d73666
LIMAM Mohamed <limam.mohamed062@gmail.com>
2015-11-13 20:26:58 +0100

    TL1-8 Test perte de vie SmartCreature en fonction des creatures autour.

--- ProjetGL/test/creatures/SmartCreatureTest.java
+++ ProjetGL/test/creatures/SmartCreatureTest.java
@@ -52,5 +52,154 @@ public class SmartCreatureTest {
 		verify(other).directionFormAPoint(eq(main.getPosition()),eq(0.0));
 		verify(other).distanceFromAPoint(eq(main.getPosition()));
 		verifyNoMoreInteractions(other);
-	}	
+	}
+	
+	/**
+	 * Test loose health alone
+	 * @throws Exception
+	 */
+	@Test
+	public void testPertePointDeVie() throws Exception {
+		SmartCreature c1 = new SmartCreature(environment, new Point2D.Double(0, 0), toRadians(0), 5, Color.RED);
+		ArrayList<ICreature> creaturesAround = new ArrayList<ICreature>();
+		when(environment.getCreatures()).thenReturn(creaturesAround);
+		
+		assertEquals(c1.getHealth(),100.0,0.001);
+		c1.act();
+		assertEquals(c1.getHealth(),99.95,0.001);
+		
+	}
+	
+	/**
+	 * Test loose health one or less than 5 creatures around
+	 * @throws Exception
+	 */
+	@Test
+	public void testPertePointDeVie1() throws Exception {
+		SmartCreature c1 = new SmartCreature(environment, new Point2D.Double(0, 0), toRadians(0), 5, Color.RED);
+		ICreature other = mock(ICreature.class);
+		when(other.getDirection()).thenReturn(toRadians(270));
+		when(other.getSpeed()).thenReturn(10.0);
+		when(other.getPosition()).thenReturn(new Point2D.Double(1,0));
+		
+		ArrayList<ICreature> creaturesAround = new ArrayList<ICreature>();
+		creaturesAround.add(other);
+
+		when(environment.getCreatures()).thenReturn(creaturesAround);
+		
+		assertEquals(c1.getHealth(),100.0,0.001);
+		c1.act();
+		assertEquals(c1.getHealth(),99.96,0.001);
+		
+	}
+	
+	/**
+	 * Test loose health more than 5 creatures around
+	 * @throws Exception
+	 */
+	@Test
+	public void testPertePointDeVie2() throws Exception {
+		SmartCreature c1 = new SmartCreature(environment, new Point2D.Double(0, 0), toRadians(0), 5, Color.RED);
+		ICreature other = mock(ICreature.class);
+		when(other.getDirection()).thenReturn(toRadians(270));
+		when(other.getSpeed()).thenReturn(10.0);
+		when(other.getPosition()).thenReturn(new Point2D.Double(1,0));
+		
+		ArrayList<ICreature> creaturesAround = new ArrayList<ICreature>();
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+
+		when(environment.getCreatures()).thenReturn(creaturesAround);
+		
+		assertEquals(c1.getHealth(),100.0,0.001);
+		c1.act();
+		assertEquals(c1.getHealth(),99.97,0.001);
+		
+	}
+	
+	/**
+	 * Test loose health more than 10 creatures around
+	 * @throws Exception
+	 */
+	@Test
+	public void testPertePointDeVie3() throws Exception {
+		SmartCreature c1 = new SmartCreature(environment, new Point2D.Double(0, 0), toRadians(0), 5, Color.RED);
+		ICreature other = mock(ICreature.class);
+		when(other.getDirection()).thenReturn(toRadians(270));
+		when(other.getSpeed()).thenReturn(10.0);
+		when(other.getPosition()).thenReturn(new Point2D.Double(1,0));
+		
+		ArrayList<ICreature> creaturesAround = new ArrayList<ICreature>();
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+
+		when(environment.getCreatures()).thenReturn(creaturesAround);
+		
+		assertEquals(c1.getHealth(),100.0,0.001);
+		c1.act();
+		assertEquals(c1.getHealth(),99.98,0.001);
+		
+	}
+	
+	/**
+	 * Test loose health more than 15 creatures around
+	 * @throws Exception
+	 */
+	@Test
+	public void testPertePointDeVie4() throws Exception {
+		SmartCreature c1 = new SmartCreature(environment, new Point2D.Double(0, 0), toRadians(0), 5, Color.RED);
+		ICreature other = mock(ICreature.class);
+		when(other.getDirection()).thenReturn(toRadians(270));
+		when(other.getSpeed()).thenReturn(10.0);
+		when(other.getPosition()).thenReturn(new Point2D.Double(1,0));
+		
+		ArrayList<ICreature> creaturesAround = new ArrayList<ICreature>();
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+		creaturesAround.add(other);
+
+		when(environment.getCreatures()).thenReturn(creaturesAround);
+		
+		assertEquals(c1.getHealth(),100.0,0.001);
+		c1.act();
+		assertEquals(c1.getHealth(),99.99,0.001);
+		
+	}
+	
+
 }
