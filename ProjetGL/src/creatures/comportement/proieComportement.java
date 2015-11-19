f4cbeafc80c36f5f870d379d4d9432be23a98fe8
LIMAM Mohamed <limam.mohamed062@gmail.com>
2015-11-19 19:17:18 +0100

    proieComportement rajout du applyNoise tester avec bouncingDeplacement c'est plus rapide

--- ProjetGL/src/creatures/comportement/proieComportement.java
+++ ProjetGL/src/creatures/comportement/proieComportement.java
@@ -1,7 +1,9 @@
 package creatures.comportement;
 
 import static commons.Utils.filter;
+import static java.lang.Math.PI;
 import static java.lang.Math.abs;
+import static java.lang.Math.random;
 
 import commons.Utils.Predicate;
 import creatures.AbstractCreature;
@@ -9,7 +11,12 @@ import creatures.CreatureComposable;
 import creatures.ICreature;
 
 public class proieComportement implements IStrategieComportement {
-
+	private static final double MAX_SPEED = 6;
+	/**
+	 * Number of cycles after which we apply some random noise.
+	 */
+	private static final int NUMBER_OF_CYCLES_PER_CHANGE = 50;
+	
 	static class CreaturesAroundCreature implements Predicate<ICreature> {
 		private final AbstractCreature observer;
 
@@ -49,6 +56,7 @@ public class proieComportement implements IStrategieComportement {
 	public void setNextDirectionAndSpeed(ICreature c) {
 		CreatureComposable c1 = (CreatureComposable)c;
 		CreatureComposable toFollow = null;
+		boolean noise = true;
 
 		Iterable<ICreature> creatures = creaturesAround(c1);
 		
@@ -56,6 +64,7 @@ public class proieComportement implements IStrategieComportement {
 			if(c1.getHealth()>c2.getHealth()){
 				toFollow = (CreatureComposable)c2;
 				c1.setDirection(c2.getDirection());
+				noise = false;
 				break;
 			}
 		}
@@ -64,8 +73,41 @@ public class proieComportement implements IStrategieComportement {
 			toFollow.setLossHealth(c1.getHealth());
 			c1.setSpeed(6);
 			toFollow.setSpeed(3);
+			noise = false;
 		}
-		c.move();
+		
+		if(noise){
+			applyNoise(c1);
+		}
+		c1.move();
+	}
+	
+	/**
+	 * Every number of cycles we apply some random noise over speed and
+	 * direction
+	 */
+	/**
+	 * Every number of cycles we apply some random noise over speed and
+	 * direction
+	 */
+	public void applyNoise(CreatureComposable c1) {
+		c1.setCurrCycle(c1.getCurrCycle()+1);
+		c1.currCycle %= NUMBER_OF_CYCLES_PER_CHANGE;
+
+		// every NUMBER_OF_CYCLES_PER_CHANGE we do the change
+		if (c1.currCycle == 0) {
+			c1.setSpeed(c1.getSpeed()+((random() * 2) - 1));
+
+			// maintain the speed within some boundaries
+			if (c1.getSpeed() < MIN_SPEED) {
+				c1.setSpeed(MIN_SPEED);
+			} else if (c1.getSpeed() > MAX_SPEED) {
+				c1.setSpeed(MAX_SPEED);
+			}
+
+			c1.setDirection(c1.getDirection()
+					+ ((random() * PI / 2) - (PI / 4)));
+		}
 	}
 
 
