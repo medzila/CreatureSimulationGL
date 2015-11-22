85e4e730c4b0ff4d935abdb4643e7a5ae7a8bedd
Finkelstein Arthur <fink.arthur@gmail.com>
2015-11-22 14:14:02 +0100

    Les scripts de deploiements

---
+++ ProjetGL/deploy.sh
@@ -0,0 +1,3 @@
+mv src/creatures/behavior/[^I]*.java myplugins/repository/creatures/behavior/;
+mv src/creatures/movement/[^I]*.java myplugins/repository/creatures/movement/;
+mv src/creatures/color/[^I]*.java myplugins/repository/creatures/color/;
---
+++ ProjetGL/undeploy.sh
@@ -0,0 +1,3 @@
+mv myplugins/repository/creatures/behavior/[^I]*.java src/creatures/behavior/;
+mv myplugins/repository/creatures/movement/[^I]*.java src/creatures/movement/;
+mv myplugins/repository/creatures/color/[^I]*.java src/creatures/color/;
